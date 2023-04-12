39c149b78ea7ed2eceaac306ae18fdc1923080b6
kejiantong <tong.ke@northeastern.edu>
2023-04-12 00:56:29 -0700

    update producer

--- Server/PostServlet/src/main/java/assignment4/postservlet/KafkaProducerFactory.java
+++ Server/PostServlet/src/main/java/assignment4/postservlet/KafkaProducerFactory.java
@@ -7,22 +7,42 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaProducerFactory {
+  private static KafkaProducerFactory instance;
+  private static Producer<String, String> producer;
+
+
+  private KafkaProducerFactory() {
+    producer = createKafkaProducer();
+  }
+
+  public static synchronized KafkaProducerFactory getInstance() {
+    if (instance == null) {
+      instance = new KafkaProducerFactory();
+    }
+    return instance;
+  }
+
+  public Producer<String, String> getKafkaProducer() {
+    return producer;
+  }
 
   public static Producer<String, String> createKafkaProducer() {
     Properties props = new Properties();
     // all these properties could be changed on demands
     // TODO: Please note we can increase the BATCH_SIZE_CONFIG,LINGER_MS_CONFIG AND BUFFER_MEMORY_CONFIG can increase throughput,
     // TODO: but may also effect latency, trade-off, we can adjust those values during performance test
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.28.39:9092,172.31.31.63:9092, 172.31.29.34:9092");
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.28.39:9092,172.31.31.63:9092");
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    props.put(ProducerConfig.ACKS_CONFIG, "all"); // TODO: this acks config could be changed on demands
-    props.put(ProducerConfig.RETRIES_CONFIG, 3);
+    props.put(ProducerConfig.ACKS_CONFIG, "1"); // TODO: this acks config could be changed on demands
+//    props.put(ProducerConfig.RETRIES_CONFIG, 1);
     props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-    props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+    props.put(ProducerConfig.LINGER_MS_CONFIG, 5); //TODO: both linger_ms_config and batch_size_config work
     props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-    props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5); // batch post to kafka
+//    props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5); // batch post to kafka
 //    props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy"); // compress the size of msg to send the broker, increase throughput
+    props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+    // Set the number of in-flight requests per connection to 1 (default for idempotent producers)
+    props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1");
 
     return new KafkaProducer<>(props);
   }
--- Server/PostServlet/src/main/java/assignment4/postservlet/SwipeServlet.java
+++ Server/PostServlet/src/main/java/assignment4/postservlet/SwipeServlet.java
@@ -20,7 +20,7 @@ public class SwipeServlet extends HttpServlet {
   @Override
   public void init() throws ServletException {
     super.init();
-    this.producer = KafkaProducerFactory.createKafkaProducer();
+    this.producer = KafkaProducerFactory.getInstance().getKafkaProducer();
   }
 
   /**
